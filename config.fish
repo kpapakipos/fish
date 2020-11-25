06995f38697bfa0b4bbbff1456e9193ec083d0b8
kpapakipos <keegan@papakipos.com>
2020-11-24 18:25:47 -0800

    feat: update aliases and fish variables to latest

--- config.fish
+++ config.fish
@@ -1,9 +1,17 @@
 alias e="code"
-alias o="e ."
-alias mp="cd ~/projects/git; find . -mindepth 1 -maxdepth 1 -type d -print -exec git -C {} pull \;"
+
+alias ca="conda activate"
+alias fr="flask run"
+alias gs="git status"
+
+alias gmp="cd ~/projects/git; find . -mindepth 1 -maxdepth 1 -type d -print -exec git -C {} pull \;"
+
 alias src="source ~/.config/fish/config.fish"
-alias edc="e -n ~/.config/fish/config.fish"
-alias home="cd ~/projects/git"
+alias efc="e -n ~/.config/fish/config.fish"
+
+alias hme="cd ~/projects/git"
+
+alias cec="conda env create --force"
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
